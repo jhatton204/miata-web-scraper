7d706eb30f95f817de5179bcf90cac18336c9e33
Jeremy Hatton <jhatton204@gmail.com>
2024-02-13 19:15:45 -0600

    udpates

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
-const puppeteer = require('puppeteer');
+const express = require('express');
+const app = express();
 const cron = require('cron');
+const puppeteer = require('puppeteer');
 const { MongoClient } = require('mongodb');
 
 // MongoDB connection URI
@@ -81,3 +83,10 @@ const job = new cron.CronJob(interval, async () => {
 
 // Start the cron job
 job.start();
+
+// Start the Express.js server
+const PORT = process.env.PORT || 10000;
+const HOST = '0.0.0.0';
+app.listen(PORT, HOST, () => {
+  console.log(`Server is running on http://${HOST}:${PORT}`);
+});
